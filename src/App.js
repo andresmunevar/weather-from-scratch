21af2ef8927ea97a327d5ab6797f7587d90626f0
andresmunevar <afmunene@gmail.com>
2021-02-13 09:33:38 -0300

    Adding my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Felipe Munevar
         </p>
         <a
           className="App-link"
